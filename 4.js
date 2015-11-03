b1c5a9fdb9bdc3ba48e3fb9ac67610cee33aba86
Thomas David Kehoe <kehoe@casafuturatech.com>
2015-11-03 13:04:45 -0700

    Refactored.

--- 4.js
+++ 4.js
@@ -1,19 +1,16 @@
 // Given a string, write a function to check if it is a permutation of a palindrome.
 // All or all but one unique characters must have an even number of instances
 
-var counterEven = 0;
-var counterOdd = 0;
 var palindromePermutation = function(string) {
+  var counterOdd = 0; // initialize counter
   string = string.toLowerCase(); // convert string to lowercase
   for (var i = 0; i < 26; i++) {
     var alpha = String.fromCharCode('a'.charCodeAt() + i); // generate the 26 lowercase letters
     var re = new RegExp( alpha, "g" ); // create a regular expression object that searches for each letter, globally
-    if (string.match(re) != null) { // if the string contains the letter
-      if (string.match(re).length%2 === 0) { // if there is an even number of a letter
-        counterEven++; // increment the count of paired letters
-      }
-      else { // odd number of characters
-        counterOdd++; // increment the count of non-paired letters
+    if (string.match(re) != null && string.match(re).length%2 != 0) { // if the string contains the letter and the count of these letters is odd
+      counterOdd++; // increment the count of paired letters
+      if (counterOdd > 1) {
+        break; // don't check the rest of the letters
       }
     }
   }
